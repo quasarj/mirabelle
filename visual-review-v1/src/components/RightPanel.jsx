7ecd93440d4b142934470d4361cb793c1ec90427
haydex <hayddex@gmail.com>
2024-04-24 12:54:39 -0500

    Components Naming v2

--- visual-review-v1/src/components/RightPanel.jsx
+++ visual-review-v1/src/components/RightPanel.jsx
@@ -1,6 +1,6 @@
 function RightPanel() {
   return (
-    <div className="w-72 rounded-lg h-full overflow-y-scroll">
+    <div id="rightPanelWrapper" className="w-72 rounded-lg h-full overflow-y-scroll">
       <div id="descriptionPanel" className="bg-gray-100 p-6 rounded-lg">
         <div className="w-full mb-2 font-semibold">Description</div>
         <div className="w-full">Heres a 4-sentence random description about the current selection or feature being reviewed, description about the current selection or feature being reviewed,description about the current selection random description about the current selection or feature being reviewed, description about the current selection or feature being reviewed,description about the current selection or feature being reviewed,providing context or additional information as required.</div>
