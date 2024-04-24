1d96ef89c8d89ce5d6d56f1c170d1fc5d14b11b6
haydex <hayddex@gmail.com>
2024-04-24 12:59:09 -0500

    Components Naming V3

--- visual-review-v1/src/components/Header.jsx
+++ visual-review-v1/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <div id="header" className=" bg-gray-100 h-12 flex items-center px-6 font-black rounded-lg">
-      <div className="flex-1">Logo</div>
-      <div className="flex-1 text-center">Visual Review</div>
-      <div className="flex-1 text-right">Username</div>
+      <div id="logo" className="flex-1">Logo</div>
+      <div id="title" className="flex-1 text-center">Visual Review</div>
+      <div id="username" className="flex-1 text-right">Username</div>
     </div>
   );
 }
