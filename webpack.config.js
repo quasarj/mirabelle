b52df0f4ee652a50c134250ee50e548d3c2e49a6
Michael Rutherford <mwrutherford@uams.edu>
2024-05-06 15:05:14 -0500

    viewer updates

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/nifti.js',
+    entry: './src/viewer.js',
     devtool: 'inline-source-map',
     devServer: {
         static: ['dist', 'public'],
