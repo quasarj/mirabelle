9c2a4cbe63f0d25f1978710d1899bfc942618e9c
haydex <hayddex@gmail.com>
2024-05-30 18:08:42 -0500

    Templates V1

--- visual-review-cornerstone-reactjs-tailwindcss-webpack-v1/src/components/Header.jsx
+++ visual-review-cornerstone-reactjs-tailwindcss-webpack-v1/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div id="header" className=" h-12 flex items-center px-6 font-black rounded-lg dark:bg-opacity-5 bg-gray-100">
       <div id="logo" className="flex-1">Logo</div>
-      <div id="title" className="flex-1 text-center">Visual Review</div>
+      <div id="title" className="flex-1 text-center">Masker</div>
       <div id="username" className="flex-1 text-right">Username</div>
     </div>
   );
--- visual-review-cornerstone-reactjs-tailwindcss-webpack-v1/src/components/ViewPanel.jsx
+++ visual-review-cornerstone-reactjs-tailwindcss-webpack-v1/src/components/ViewPanel.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import CornerstoneViewer from './CornerstoneViewer';
 
-function ViewPanel({ zoom, opacity }) {
+function ViewPanel({ zoom, opacity, layout }) {
   return (
     <div id="viewPanel" className="flex text-center gap-2 h-my-2 flex-grow overflow-hidden">
-        <CornerstoneViewer zoom={zoom} opacity={opacity} />
+        <CornerstoneViewer zoom={zoom} opacity={opacity} layout={layout} />
     </div>
   );
 }
