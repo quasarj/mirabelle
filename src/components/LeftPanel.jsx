19a182fc1a5899e68fcbbe82cff2b028560e9e04
haydex <hayddex@gmail.com>
2024-07-01 14:05:53 -0500

    Side panels hide/show V2

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -8,7 +8,7 @@ function LeftPanel({ setZoom, setOpacity }) {
   const { layout } = useContext(Context);
   
   return (
-    <div id="leftPanelWrapper" className="grid grid-rows-[auto,auto] h-full overflow-hidden gap-2">
+    <div id="leftPanelWrapper" className="grid grid-rows-[auto,auto] h-full w-72 overflow-hidden gap-2">
       {layout !== 'Masker' && layout !== 'MaskerVR' ? <FilesPanel /> : null }
       <FilesPanel />
       <ToolsPanel setZoom={setZoom} setOpacity={setOpacity} />
--- src/components/Masker.jsx
+++ src/components/Masker.jsx
@@ -17,33 +17,39 @@ function Masker({ files, iecs, iec }) {
   console.log('layout', layout);
 
   const gridTemplate = leftPanelVisibility && rightPanelVisibility
-    ? 'grid-cols-[auto,1fr,auto]'
+    ? 'grid-cols-[18rem,1fr,18rem]'
     : leftPanelVisibility 
-    ? 'grid-cols-[auto,1fr]'
+    ? 'grid-cols-[18rem,1fr,0rem]'
     : rightPanelVisibility 
-    ? 'grid-cols-[1fr,auto]'
-    : 'grid-cols-1';
+    ? 'grid-cols-[0rem,1fr,18rem]'
+    : 'grid-cols-[0rem,1fr,0rem]';
   
   return (
     <div id="app" className={`grid grid-rows-[auto,1fr] gap-2 w-screen h-screen p-2 dark:bg-blue-950`}>
       <Header />
       {/*{topPanelVisibility && <TopPanel />}*/}
-      <div id="main" className={`h-full grid ${ gridTemplate } rounded-lg gap-2 overflow-hidden transition-all duration-300`}>
-        {leftPanelVisibility && (
+      <div id="main" className={`h-full w-full grid ${ gridTemplate } rounded-lg gap-2 overflow-hidden transition-all duration-300`}>
+        <div id="leftPanel" className={`w-full h-full rounded-lg overflow-hidden`}>
+          <LeftPanel />
+        </div>
+        {/*{leftPanelVisibility && (
           <div id="leftPanel" className={`w-72 h-full rounded-lg overflow-y-hidden ${leftPanelVisibility ? 'slide-in' : 'slide-out'}`} >
             <LeftPanel />
           </div>
-        )}
+        )}*/}
         <MiddlePanel
           files={files}
           iecs={iecs}
           iec={iec}
         />
-        {rightPanelVisibility && (
+        <div id="rightPanel" className="w-full h-full rounded-lg overflow-hidden">
+          <RightPanel />
+        </div>
+        {/*{rightPanelVisibility && (
           <div id="rightPanel" className="w-72 h-full rounded-lg overflow-hidden">
             <RightPanel />
           </div>
-        )}
+        )}*/}
       </div>
     </div>
   )
