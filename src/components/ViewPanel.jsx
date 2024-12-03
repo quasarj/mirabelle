68de011f49eebf8e85621f8e3cd3c008f6f8fac9
Michael Rutherford <mwrutherford@uams.edu>
2024-12-03 06:03:38 -0600

    Split viewer into separate stack and volume panels

--- src/components/ViewPanel.jsx
+++ src/components/ViewPanel.jsx
@@ -1,18 +1,31 @@
-import React, { forwardRef, useRef } from 'react';
-import CornerstoneViewer from './CornerstoneViewer';
+import React, { forwardRef, useRef, useContext } from 'react';
+import { Context } from './Context.js';
+//import CornerstoneViewer from './CornerstoneViewer';
+import ViewStackPanel from './ViewStackPanel';
+import ViewVolumePanel from './ViewVolumePanel';
 
 function ViewPanel({ files, volumeName, iec }) {
-  return (
-    <div id="viewPanel" className="grid grid-rows-[1fr,auto] text-center gap-2 overflow- h-full">
-	  {files && files.length > 0 &&
-        <CornerstoneViewer 
-          files={files}
-          volumeName={volumeName}
-          iec={iec}
-        />
-	  }
-    </div>
-  );
+
+    const { viewport_layout } = useContext(Context);
+
+    return (
+        <div id="viewPanel" className="grid grid-rows-[1fr,auto] text-center gap-2 overflow- h-full">
+            {viewport_layout == 'stack' && files && files.length > 0 &&
+                <ViewStackPanel
+                    files={files}
+                    volumeName={volumeName}
+                    iec={iec}
+                />
+            }
+            {viewport_layout == 'volume' && files && files.length > 0 &&
+                <ViewVolumePanel
+                    files={files}
+                    volumeName={volumeName}
+                    iec={iec}
+                />
+            }
+        </div>
+    );
 };
 
 export default ViewPanel;
