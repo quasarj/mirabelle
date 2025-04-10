24849208d06b9138a15717363daf612b357ab26f
Michael Rutherford <mwrutherford@uams.edu>
2025-04-09 22:33:37 -0500

    Updated config and state management

--- src/features/presentationSlice.js
+++ src/features/presentationSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const presentationSlice = createSlice({
   name: 'presentation',
   initialState: {
-
+    // Panel Configuration
     panelConfig: {
       visibility: {
         left: false,
@@ -21,6 +21,7 @@ const presentationSlice = createSlice({
         right: false,
       },
     },
+    // Tools Configuration
     toolsConfig: {
       viewToolGroup: {
         visible: false,
@@ -65,20 +66,67 @@ const presentationSlice = createSlice({
           pan: false,
         }
       },
-      opacityTool: {
+      opacityToolGroup: {
         visible: false,
         min: 0,
         max: 1,
         step: 0.01,
         defaultValue: 0.3,
       },
-      presetTool: {
+      presetToolGroup: {
         visible: false,
         defaultValue: 'CT-MIP',
       },
+      resetToolGroup: {
+        visible: false,
+        defaultValue: false,
+      },
+    },
+    // Button Panel Configuration
+    buttonConfig: {
+      masker: {
+        visible: false,
+        visibility: {
+          expand: false,
+          clear: false,
+          accept: false,
+        },
+      },
+      maskerReview: {
+        visible: false,
+        visibility: {
+          accepted: false,
+          rejected: false,
+          skip: false,
+          nonMaskable: false,
+        },
+      },
+      visualReview: {
+        visible: false,
+        visibility: {
+          good: false,
+          bad: false,
+          blank: false,
+          scout: false,
+          other: false,
+          flag: false,
+        },
+      },
     },
 
-
+    // State Values
+    stateValues: {
+      left: false,
+      right: false,
+      reset: false,
+      view: 'volume',
+      function: 'mask',
+      form: 'cylinder',
+      leftClick: 'selection',
+      rightClick: 'zoom',
+      opacity: 0.3,
+      preset: 'CT-MIP',
+    },
 
     maximumIntensityProjection: false,
 
@@ -115,6 +163,38 @@ const presentationSlice = createSlice({
     setMip: (state, action) => {
       state.maximumIntensityProjection = action.payload
     },
+
+    setLeft: (state, action) => {
+      state.stateValues.left = action.payload
+    },
+    setRight: (state, action) => {
+      state.stateValues.right = action.payload
+    },
+    setReset: (state, action) => {
+      state.stateValues.reset = action.payload
+    },
+    setView: (state, action) => {
+      state.stateValues.view = action.payload
+    },
+    setFunction: (state, action) => {
+      state.stateValues.function = action.payload
+    },
+    setForm: (state, action) => {
+      state.stateValues.form = action.payload
+    },
+    setLeftClick: (state, action) => {
+      state.stateValues.leftClick = action.payload
+    },
+    setRightClick: (state, action) => {
+      state.stateValues.rightClick = action.payload
+    },
+    setOpacity: (state, action) => {
+      state.stateValues.opacity = action.payload
+    },
+    setPreset: (state, action) => {
+      state.stateValues.preset = action.payload
+    },
+
     // Set the entire toolsConfig
     setToolsConfig: (state, action) => {
       state.toolsConfig = { ...state.toolsConfig, ...action.payload }
@@ -130,12 +210,141 @@ const presentationSlice = createSlice({
     removePreset: (state, action) => {
       state.presets = state.presets.filter(preset => preset !== action.payload)
     },
-    // Sets the default configuration for the Masker Route
+
+
+    // Sets the configuration for the Masker Route
     setMaskerConfig: (state, action) => {
       state.panelConfig.visibility.left = true;
       state.panelConfig.visibility.tools = true;
+      state.panelConfig.visibility.reset = true;
+
       state.panelConfig.open.left = true;
+
       state.toolsConfig.functionToolGroup.visible = true;
+      state.toolsConfig.formToolGroup.visible = true;
+
+      state.toolsConfig.leftClickToolGroup.visible = true;
+      state.toolsConfig.leftClickToolGroup.defaultValue = 'selection';
+      state.toolsConfig.leftClickToolGroup.visibility.windowLevel = true;
+      state.toolsConfig.leftClickToolGroup.visibility.rectangleScissors = true;
+
+      state.toolsConfig.rightClickToolGroup.visible = true;
+      state.toolsConfig.rightClickToolGroup.defaultValue = 'zoom';
+      state.toolsConfig.rightClickToolGroup.visibility.zoom = true;
+      state.toolsConfig.rightClickToolGroup.visibility.pan = true;
+
+      state.buttonConfig.masker.visible = true;
+      state.buttonConfig.masker.visibility.expand = true;
+      state.buttonConfig.masker.visibility.clear = true;
+      state.buttonConfig.masker.visibility.accept = true;
+
+      return state;
+    },
+
+    // Sets the configuration for the Masker Review Route
+    setMaskerReviewConfig: (state, action) => {
+      state.panelConfig.visibility.left = true;
+      state.panelConfig.visibility.tools = true;
+      state.panelConfig.visibility.reset = true;
+
+      state.panelConfig.open.left = true;
+
+      state.toolsConfig.leftClickToolGroup.visible = true;
+      state.toolsConfig.leftClickToolGroup.defaultValue = 'windowlevel';
+      state.toolsConfig.leftClickToolGroup.visibility.windowLevel = true;
+
+      state.toolsConfig.rightClickToolGroup.visible = true;
+      state.toolsConfig.rightClickToolGroup.defaultValue = 'zoom';
+      state.toolsConfig.rightClickToolGroup.visibility.zoom = true;
+      state.toolsConfig.rightClickToolGroup.visibility.pan = true;
+
+      state.buttonConfig.maskerReview.visible = true;
+      state.buttonConfig.maskerReview.visibility.accepted = true;
+      state.buttonConfig.maskerReview.visibility.rejected = true;
+      state.buttonConfig.maskerReview.visibility.skip = true;
+      state.buttonConfig.maskerReview.visibility.nonMaskable = true;
+
+      return state;
+    },
+
+    // Sets the configuration for the Visual Review Route
+    setVisualReviewConfig: (state, action) => {
+      state.panelConfig.visibility.left = true;
+      state.panelConfig.visibility.right = true;
+      state.panelConfig.visibility.tools = true;
+      state.panelConfig.visibility.reset = true;
+      state.panelConfig.visibility.description = true;
+
+      state.panelConfig.open.left = true;
+      state.panelConfig.open.right = true;
+
+      state.toolsConfig.leftClickToolGroup.visible = true;
+      state.toolsConfig.leftClickToolGroup.defaultValue = 'windowlevel';
+      state.toolsConfig.leftClickToolGroup.visibility.windowLevel = true;
+
+      state.toolsConfig.rightClickToolGroup.visible = true;
+      state.toolsConfig.rightClickToolGroup.defaultValue = 'zoom';
+      state.toolsConfig.rightClickToolGroup.visibility.zoom = true;
+      state.toolsConfig.rightClickToolGroup.visibility.pan = true;
+
+      state.buttonConfig.visualReview.visible = true;
+      state.buttonConfig.visualReview.visibility.good = true;
+      state.buttonConfig.visualReview.visibility.bad = true;
+      state.buttonConfig.visualReview.visibility.blank = true;
+      state.buttonConfig.visualReview.visibility.scout = true;
+      state.buttonConfig.visualReview.visibility.other = true;
+      state.buttonConfig.visualReview.visibility.flag = true;
+
+      return state;
+    },
+
+    // Sets the configuration for Stacks
+    setStackConfig: (state, action) => {
+      state.toolsConfig.viewToolGroup.visible = true;
+      state.toolsConfig.viewToolGroup.defaultValue = 'stack';
+      state.toolsConfig.viewToolGroup.visibility.stack = true;
+
+      state.toolsConfig.functionToolGroup.defaultValue = 'blackout';
+      state.toolsConfig.functionToolGroup.visibility.blackout = true;
+
+      state.toolsConfig.formToolGroup.defaultValue = 'cuboid';
+      state.toolsConfig.formToolGroup.visibility.cuboid = true;
+
+      state.buttonConfig.masker.visibility.expand = false;
+
+      return state;
+    },
+
+    // Sets the configuration for Stacks
+    setVolumeConfig: (state, action) => {
+      state.toolsConfig.viewToolGroup.visible = true;
+      state.toolsConfig.viewToolGroup.defaultValue = 'volume';
+      state.toolsConfig.viewToolGroup.visibility.volume = true;
+      state.toolsConfig.viewToolGroup.visibility.projection = true;
+      state.toolsConfig.viewToolGroup.visibility.stack = false; // need capability to switch to stack, but not now
+
+      state.toolsConfig.functionToolGroup.defaultValue = 'mask';
+      state.toolsConfig.functionToolGroup.visibility.mask = true;
+      state.toolsConfig.functionToolGroup.visibility.blackout = true;
+      state.toolsConfig.functionToolGroup.visibility.sliceRemove = true;
+
+      state.toolsConfig.formToolGroup.defaultValue = 'cylinder';
+      state.toolsConfig.formToolGroup.visibility.cuboid = true;
+      state.toolsConfig.formToolGroup.visibility.cylinder = true;
+
+      state.toolsConfig.leftClickToolGroup.visibility.crossHairs = true;
+
+      state.toolsConfig.opacityToolGroup.visible = true;
+      state.toolsConfig.presetToolGroup.visible = true;
+
+      return state;
+    },
+
+    // Sets the configuration for Stacks
+    setNiftiConfig: (state, action) => {
+      state.presetToolGroup.defaultValue = 'MR-Default';
+      state.buttonConfig.visualReview.visibility.flag = false;
+
       return state;
     },
 
