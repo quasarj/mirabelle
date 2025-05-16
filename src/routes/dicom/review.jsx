e9605992c9f6318d8b16d160c7bad3432e44af77
Quasar Jarosz <quasar@uams.edu>
2025-05-15 19:15:05 -0500

    Fix bad gitignore and rerun prettier on all
    Turns out Prettier thought it was being clever by ignoring
    things in the .gitignore, and we had dicom and nifti in there
    for some reason.

--- src/routes/dicom/review.jsx
+++ src/routes/dicom/review.jsx
@@ -1,51 +1,51 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import MainPanel from '../../components/MainPanel.jsx';
-import { Context } from '../../components/Context';
-import useConfigState from '../../hooks/useConfigState';
-import { getDicomDetails } from '../../visualreview.js';
-import { getFiles, getIECInfo } from '../../utilities';
-import { TASK_CONFIGS } from '../../config/config';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import MainPanel from "../../components/MainPanel.jsx";
+import { Context } from "../../components/Context";
+import useConfigState from "../../hooks/useConfigState";
+import { getDicomDetails } from "../../visualreview.js";
+import { getFiles, getIECInfo } from "../../utilities";
+import { TASK_CONFIGS } from "../../config/config";
 
 import Toast from "../../components/Toast";
 
 export async function loader({ params }) {
-
-    const details = await getDicomDetails(params.iec);
-    const fileInfo = await getIECInfo(params.iec);
-    return { details, fileInfo, iec: params.iec };
+  const details = await getDicomDetails(params.iec);
+  const fileInfo = await getIECInfo(params.iec);
+  return { details, fileInfo, iec: params.iec };
 }
 
 export default function ReviewDICOM() {
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState("initial message");
 
-    const { details, fileInfo, iec } = useLoaderData();
+  const { details, fileInfo, iec } = useLoaderData();
 
-    let configState;
+  let configState;
 
-    // Use specific config for this route, fallback to 'default' if not found
-    if (fileInfo.volumetric) {
-        configState = useConfigState(TASK_CONFIGS.dicom_review_volume || TASK_CONFIGS.default);
-    } else {
-        configState = useConfigState(TASK_CONFIGS.dicom_review_stack || TASK_CONFIGS.default);
-    }
+  // Use specific config for this route, fallback to 'default' if not found
+  if (fileInfo.volumetric) {
+    configState = useConfigState(
+      TASK_CONFIGS.dicom_review_volume || TASK_CONFIGS.default,
+    );
+  } else {
+    configState = useConfigState(
+      TASK_CONFIGS.dicom_review_stack || TASK_CONFIGS.default,
+    );
+  }
 
   configState.showToast = (message) => {
-    setToastMessage(message)
-    setShowToast(true)
+    setToastMessage(message);
+    setShowToast(true);
   };
 
-    // Here we just assemble the various panels that we need for this mode
-    return (
-        <Context.Provider value={{ ...configState }}>
-            <MainPanel details={details} files={fileInfo.frames} iec={iec} />
-            {showToast && (
-                <Toast
-                    message={toastMessage}
-                    onClose={() => setShowToast(false)}
-                />
-            )}
-        </Context.Provider>
-    );
+  // Here we just assemble the various panels that we need for this mode
+  return (
+    <Context.Provider value={{ ...configState }}>
+      <MainPanel details={details} files={fileInfo.frames} iec={iec} />
+      {showToast && (
+        <Toast message={toastMessage} onClose={() => setShowToast(false)} />
+      )}
+    </Context.Provider>
+  );
 }
