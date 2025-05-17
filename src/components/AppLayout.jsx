2dc76ca8823f5b9b0d7ff958d1774cf98dcd5e11
Quasar Jarosz <quasar@uams.edu>
2025-05-17 18:28:22 -0500

    Add react-hot-toast dep

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import './AppLayout.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import Header from "./Header";
+
+import "./AppLayout.css";
 
 export default function AppLayout() {
-    return (
-        <div id="app">
-            <Header />
-            <Outlet />
-        </div>
-    );
+  return (
+    <div id="app">
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: "1.5rem",
+          },
+        }}
+      />
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
